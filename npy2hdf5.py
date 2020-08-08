6e29b7523c24a282264318e9d9b7bd3918587860
pardoalejo <pardogl@login510-27.ibex.kaust.edu.sa>
2020-08-08 18:06:08 +0300

    Function

--- npy2hdf5.py
+++ npy2hdf5.py
@@ -3,22 +3,33 @@ import numpy as np
 import os.path as osp
 import glob
 import tqdm
+import argparse
 
-source = 'youtube'
-features_path = (f'~/scratch/data/movies/movies/{source}/')
-feat_type = 'audio'
-videos = glob.glob(f'{features_path}/*/*{feat_type}.npy')
-print(f'{len(videos)} features found')
-features = []
-names = []
-for video in tqdm.tqdm(videos):
-    feature = np.load(open(video,'rb'))
-    features.append(feature)
-    name = osp.basename(video).replace('.npy','')
-    names.append(name)
+def get_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '--feat_type',
+        default='audio',
+        type=str)
+    return parser.parse_args()
 
-print('Saving hdf5 file')
-with h5.File(osp.join(features_path, f'../ResNet-18_{source}_{feat_type}_features.h5'),'w') as f:
-    for name, feature in tqdm.tqdm(zip(names, features), total=len(names)):
-        f.create_dataset(name, data=feature, chunks=True)
+def npy2h5(feat_type='audio'):
+    features_path = ('/home/pardogl/scratch/data/movies/youtube/*')
+    videos = glob.glob(f'{features_path}/*_{feat_type}.npy')
+    print(f'{len(videos)} features found')
+    features = []
+    names = []
+    for video in tqdm.tqdm(videos):
+        feature = np.load(open(video,'rb'))
+        features.append(feature)
+        name = osp.basename(video).replace('.npy','')
+        names.append(name)
 
+    print('Saving hdf5 file')
+    with h5.File(osp.join(features_path, f'/home/pardogl/scratch/data/movies/ResNet-18_{feat_type}_features.h5'),'w') as f:
+        for name, feature in tqdm.tqdm(zip(names, features), total=len(names)):
+            f.create_dataset(name, data=feature, chunks=True)
+
+if __name__ == "__main__":
+    args = get_arguments()
+    npy2h5(args.feat_type)
