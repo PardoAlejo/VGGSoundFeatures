ebc6038ed7e9bf3b46899437d5d6242def43ce78
pardoalejo <alejandro.pardo@kaust.edu.sa>
2021-09-26 15:07:21 +0300

    Collect results to h5py

---
+++ collect_results.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+python npy2hdf5.py --features_path ../../datasets/movies/unedited-set/features/ --suffix audio_16 --out_path ../../datasets/movies/unedited-set/ --out_name ResNet-18_audio_features_unedited
